39b56e4d099ef8d32953beb8393fea03a151f571
VLuca997 <kulertoast997@gmail.com>
2023-06-26 18:23:24 +0200

    Add Object Array

---
+++ js/script.js
@@ -0,0 +1,34 @@
+//Array di oggetti:
+
+const membersTeam = [
+    {
+        name: "Wayne Bernett",
+        position: "Founder & CEO",
+        photo: "wayne-barnett-founder-ceo.jpg",
+    },
+    {
+        name: "Angela Caroll",
+        position: "Chief Editor",
+        photo: "angela-caroll-chief-editor.jpg",
+    },
+    {
+        name: "Walter Gordon",
+        position: "Office Manager",
+        photo: "walter-gordon-office-manager.jpg",
+    },
+    {
+        name: "Angela Lopez",
+        position: "Social Media Manager",
+        photo: "angela-lopez-social-media-manager.jpg",
+    },
+    {
+        name: "Scott Estrada",
+        position: "Developer",
+        photo: "scott-estrada-developer.jpg",
+    },
+    {
+        name: "Barbara Ramos",
+        position: "Graphic Designer",
+        photo: "barbara-ramos-graphic-designer.jpg",
+    },
+];
